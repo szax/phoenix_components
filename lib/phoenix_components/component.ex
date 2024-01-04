c310773f6509e07c7a996c5b591e557167e8066c
Martin Jablonski <szax22@gmail.com>
2024-01-04 15:33:20 +0100

    Update component.ex

--- lib/phoenix_components/component.ex
+++ lib/phoenix_components/component.ex
@@ -41,8 +41,11 @@ defmodule PhoenixComponents.Component do
   defmacro __using__(_opts) do
     quote do
       use Phoenix.View, root: Application.get_env(:phoenix_components, :path, "web")
-      use Phoenix.HTML
-      use PhoenixComponents.View
+      import Phoenix.HTML
+      import Phoenix.HTML.Form
+      use PhoenixHTMLHelpers
+
+      import Phoenix.Component
     end
   end
 end
